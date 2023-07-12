7678f14d943aca30db86369cc8095241c7e05859
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-12 14:52:09 +0300

    bonus1 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -208,4 +208,21 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
     console.log('someone else should drive..');
 }
 
+
+
+// Bonus1
+
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy');
+} else if (scoreDolphins < scoreKoalas) {
+    console.log('Koalas win the trophy');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy.')
+}
 */
