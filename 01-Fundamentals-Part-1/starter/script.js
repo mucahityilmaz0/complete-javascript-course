0658b125d63632accee455941c87b3dc6242a73c
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-12 16:36:57 +0300

    The switch Statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -225,4 +225,53 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
     console.log('No one wins the trophy.')
 }
+
+
+const day = 'saturday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('prepare theory videos');
+//         break;
+//     case 'wednesday':
+//         break;
+//     case 'thursday':
+//         console.log('Write code examples');
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//         break;
+//     case 'sunday':
+//         console.log('enjoy the weekend');
+//         break;
+//     default:
+//         console.log('not a valid day!');
+//         break;
+// }
+
+//if else statement
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('prepare theory videos');
+} else if (day === 'wednesday') {
+    console.log('Write code examples');
+} else if (day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday') {
+    console.log('enjoy the weekend');
+} else if (day === 'sunday') {
+    console.log('enjoy the weekend');
+} else {
+    console.log('not a valid day!');
+}
 */
