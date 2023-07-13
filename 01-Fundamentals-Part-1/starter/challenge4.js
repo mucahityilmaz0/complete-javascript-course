e192dc7967152b8301a2565c817d4d20fb95dc37
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-13 11:47:46 +0300

    fixed and optimization

--- 01-Fundamentals-Part-1/starter/challenge4.js
+++ 01-Fundamentals-Part-1/starter/challenge4.js
@@ -1,3 +1,3 @@
 const bill = 275;
-const tip = bill > 50 && bill < 300 ? bill * 15 / 100 : bill * 20 / 100;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
