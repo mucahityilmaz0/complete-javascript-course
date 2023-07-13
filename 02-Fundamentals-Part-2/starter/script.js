4122a2a2b1a60b08f937993670c9fdf0537206dc
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-13 15:14:01 +0300

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,3 +9,30 @@ if (hasDriversLicense) console.log('I can drive.');
 //const interface = 'Audio';
 const private = 556;
 */
+
+function logger() {
+    console.log('My name is Mücahit');
+}
+
+// calling , running , invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//console.log(fruitProcessor(2, 4));
+
+const num = Number('23');
