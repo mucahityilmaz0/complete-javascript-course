87f689a391e7b1a20bc3a056db55ece195bd2b60
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-14 11:36:41 +0300

    change the variable name

--- 02-Fundamentals-Part-2/starter/challenge1.js
+++ 02-Fundamentals-Part-2/starter/challenge1.js
@@ -11,11 +11,11 @@ const scoreKoalas = calcAverage(65, 54, 49);
 const scoreDolphins1 = calcAverage(85, 54, 41);
 const scoreKoalas1 = calcAverage(23, 34, 27);
 
-function checkWinner(scoreDolphins, scoreKoalas) {
-    if (scoreDolphins > scoreKoalas && scoreDolphins >= scoreKoalas * 2) {
-        console.log(`Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
-    } else if (scoreKoalas > scoreDolphins && scoreKoalas >= scoreDolphins * 2) {
-        console.log(`Koalas win (${scoreKoalas} vs. ${scoreDolphins})`);
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins > avgKoalas && avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas > avgDolphins && avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
         console.log('No team wins...');
     }
