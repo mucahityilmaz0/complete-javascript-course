61f30d78a3739fee63ccbf15a4e7072d30a0116c
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-18 12:02:14 +0300

    bug debug

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -54,7 +54,7 @@ console.log(amplitude);
 // 2) Breaking up into sub-problems
 // - Merge 2 arrays
 
-const calcTempAmplitudeNew = function (t1, t2) {
+const calcTempAmplitudeBug = function (t1, t2) {
   const temps = t1.concat(t2);
   console.log(temps);
 
@@ -71,6 +71,42 @@ const calcTempAmplitudeNew = function (t1, t2) {
   console.log(max, min);
   return max - min;
 };
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-console.log(amplitudeNew);
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeBug);
+
+
+const measureKelvin = function () {
+  const measurement ={
+    type: 'temp',
+    unit: 'celsius',
+    value:Number(prompt('Degrees celsius:'))
+  }
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+}
+
+console.log(measureKelvin());
+
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
+
 */
