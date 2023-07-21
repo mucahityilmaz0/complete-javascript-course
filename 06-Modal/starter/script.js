c0c30ab8718229f4324d4cde666e5081311e6801
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-21 11:17:26 +0300

    Working With Classes and Handling an Esc Keypress Event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,8 +5,33 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal= document.querySelectorAll('.show-modal');
 
-console.log(btnsShowModal);
+function openModel() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModel() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+//console.log(btnsShowModal);
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-    console.log(btnsShowModal[i].textContent);
+    btnsShowModal[i].addEventListener('click',openModel);
+    // btnsShowModal[i].addEventListener('click', function(){
+    //     console.log('Button clicked '+ (i+1));
+    //     modal.classList.remove('hidden');
+    //     overlay.classList.remove('hidden');
+    // });
 }
+
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function(e){
+    if (e.key==='Escape') {
+        if(!modal.classList.contains('hidden')){
+            closeModel();
+        }
+    }
+});
