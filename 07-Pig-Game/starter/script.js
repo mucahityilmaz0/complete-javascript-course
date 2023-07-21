68ef41bd18f5ef165ba80731f27b52ef47fd136a
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-21 13:48:00 +0300

    Pig Game starter position added

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1'); //faster than query
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add('hidden');
