e85495c05855fdf1607eeb6d1a808299c3a807c6
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-23 18:32:29 +0300

    Switching the Active Player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1'); //faster than query
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const name0EL = document.getElementById('name--0');
+const name1EL = document.getElementById('name--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -16,7 +20,9 @@ score0El.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice funcionality
 btnRoll.addEventListener('click', function () {
@@ -30,8 +36,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
