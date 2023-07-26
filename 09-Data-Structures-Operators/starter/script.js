e343062092eed0fcc3516e8975215fafd8226960
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-26 15:18:49 +0300

    Short Circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,6 +50,37 @@ const restaurant = {
 };
 
 /*
+
+//Short Circuiting
+console.log('----------OR----------');
+console.log(3 || 'mucahit');
+console.log('' || 'mucahit');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('----------AND----------');
+
+console.log(0 && 'Mucahit');
+console.log(7 && 'Mucahit');
+
+console.log('Hello' && 23 && null && 'Mucahit');
+
+//Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
 //Rest Pattern and Parameters
 //1)Desctucturing
 
