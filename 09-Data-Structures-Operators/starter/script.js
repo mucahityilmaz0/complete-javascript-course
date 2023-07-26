10274e1b1a0847c23ceaab0fe3cfed321c1d690d
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-07-26 14:09:23 +0300

    Rest Pattern and Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -42,9 +42,54 @@ const restaurant = {
       `Here is your delicipus pasta with ${ing1}, ${ing2} and ${ing3}. `
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 /*
+//Rest Pattern and Parameters
+//1)Desctucturing
+
+//Spread, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+console.log(arr);
+
+//Rest, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//Object
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'onion', 'olive', 'spinach');
+restaurant.orderPizza('mushrooms');
+
 /////////////////////////////////////////////////
 //The spread operator ...
 
