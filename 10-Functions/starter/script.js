e46b0575f94b5dee7ac03d394226062070ac42d6
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-01 12:08:38 +0300

    How Passing Arguments Works

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-/*
+/* 
 const bookings = [];
 
 const createBooking = function (
@@ -26,4 +26,37 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 234222345567,
+};
+
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+    
+    if (passenger.passport === 234222345567) {
+        alert('Checked in');
+    } else {
+        alert('Wrong passport!');
+    }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// //Is the same as doing..
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
 */
