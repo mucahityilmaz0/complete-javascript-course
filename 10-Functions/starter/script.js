0ff8a469fe1d7c5b6f5b8de9005ad8d66b369789
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-02 11:03:26 +0300

    Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,4 +59,33 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('🤚');
+};
+
+document.body.addEventListener('click', high5);
+
+['jonas', 'martha', 'adam'].forEach(high5);
 */
