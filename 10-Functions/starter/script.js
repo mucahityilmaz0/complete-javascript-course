a248c5d9d0249c505ad9f2ad8d3f830246db9db4
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-03 11:22:31 +0300

    The bind Method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -115,23 +115,23 @@ const lufthansa = {
       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
       );
       this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
-
-lufthansa.book(239, 'jonas schmedtmann');
-lufthansa.book(635, 'john Smith');
-console.log(lufthansa);
-
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-const book = lufthansa.book;
-
-//call method
-//book(23, 'Sarah Williams');
+    },
+  };
+  
+  lufthansa.book(239, 'jonas schmedtmann');
+  lufthansa.book(635, 'john Smith');
+  console.log(lufthansa);
+  
+  const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+  };
+  
+  const book = lufthansa.book;
+  
+  //call method
+  //book(23, 'Sarah Williams');
 book.call(eurowings, 23, 'Sarah Williams');
 book.call(lufthansa, 33, 'Mucahit Yilmaz');
 
@@ -148,5 +148,47 @@ const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 
 book.call(swiss, ...flightData);
+
+//bind method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmetmann');
+
+//with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+//lufthansa.buyPlane();
+document
+.querySelector('.buy')
+.addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+
+//Challenge
+function addTaxChal(rate, value) {
+  return value + value * rate;
+}
+
+function addVATChal(value) {
+  const rate = 0.23;
+  return addTaxChal(rate, value);
+}
+
+console.log(addVATChal(100));
 
 */
