6f796a3d182cb83ee2987318ecb64b8aac59524a
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-07 17:47:01 +0300

    The new at Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -100,9 +100,18 @@ console.log(arr2);
 
 //Concat
 const letters = arr.concat(arr2);
+
 console.log(letters);
 console.log([...arr, ...arr2]);
 
 //Join
 console.log(letters.join(' - '));
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
 */
