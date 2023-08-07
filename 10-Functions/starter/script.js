db92c91bd0fa5d713d807a72aaf5be3c478ec723
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-07 14:56:10 +0300

    More Closure Examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -227,4 +227,45 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
-*/
+
+//example1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+//re-assinging f function
+h();
+f();
+
+//Example2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+// setTimeout(function () {
+  //   console.log('TIMER');
+  // }, 1000);
+  
+  */
