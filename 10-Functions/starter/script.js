27e46b18ffa9485d581cce803011b77583faafbe
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-07 14:38:03 +0300

    Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -211,4 +211,20 @@ runOnce();
 console.log(notPrivate);
 //console.log(isPrivate);
 
+
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
 */
