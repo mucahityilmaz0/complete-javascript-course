891340b8b7b49f8a23cca8ab641884b0f07990cf
mucahityilmaz0 <390925@ogr.ktu.edu.tr>
2023-08-18 14:55:47 +0300

    More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -518,5 +518,35 @@ const sortMovementRL = movements.sort(function (a, b) {
 });
 
 console.log(sortMovementRL);
+More Ways of Creating and Filling Arrays
 
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+console.log(x.map(() => 5));
+
+//x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, function (_, i) {
+  return i + 1;
+});
+console.log(z);
 */
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  //console.log(movementsUI);
+});
